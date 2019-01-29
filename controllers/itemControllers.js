b95f9e980b468a70faa172cca7a6795d79280596
Panjapol Sanguanpanich <panjaphol@hotmail.com>
2019-01-29 17:21:17 +0700

    Update search api

--- controllers/itemControllers.js
+++ controllers/itemControllers.js
@@ -17,21 +17,23 @@ const getDetailItem = (req, res) => {
 }
 
 const searchItems = (req, res) => {
-    let text = req.body.trim()
+    let text = req.body.text.trim()
+    console.log(text)
     let searchResults = []
     let query = 'SELECT * FROM Item'
     connection.query(query, (error, results) => {
         if (error) throw error
         let resultsArray = JSON.parse(JSON.stringify(results))
-        resultsArray.array.forEach(item => {
+        resultsArray.forEach(item => {
             if (
                 item['ItemBrand'].includes(text) ||
-                item['ItemDesc'].includes(text)
+                item['ItemDesc'].includes(text) ||
+                item['ItemColor'].includes(text)
             ) {
                 searchResults.push(item)
             }
         })
-        res.status(200).json(results)
+        res.status(200).json(searchResults)
     })
 }
 
