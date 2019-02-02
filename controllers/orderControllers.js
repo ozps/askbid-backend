4876a483fcedb5ce9635fe4fcf5448636fb64a90
Panjapol Sanguanpanich <panjaphol@hotmail.com>
2019-02-02 15:48:47 +0700

    Add outline of order api

---
+++ controllers/orderControllers.js
@@ -0,0 +1,22 @@
+const connection = require('../models/dbConnection')
+
+const createOrder = (req, res) => {}
+
+const getDetailOrder = (req, res) => {}
+
+const getAskPrice = (req, res) => {}
+
+const getBidPrice = (req, res) => {}
+
+const updateOrder = (req, res) => {}
+
+const deleteOrder = (req, res) => {}
+
+module.exports = {
+    createOrder,
+    getDetailOrder,
+    getAskPrice,
+    getBidPrice,
+    updateOrder,
+    deleteOrder
+}
--- routes/orderRoutes.js
+++ routes/orderRoutes.js
@@ -1,6 +1,23 @@
-set_order
-get_order
-get_ask_price
-get_bid_price
-remove_order
-update_order
+const { Router } = require('express')
+const orderControllers = require('../controllers/orderControllers')
+const router = new Router()
+
+// Create bid or ask order
+router.post('/create_order', orderControllers.createOrder)
+
+// Get bid or ask order
+router.get('/get_order/:id', orderControllers.getDetailOrder)
+
+// Get ask item price
+router.get('/get_ask_price/:id', orderControllers.getAskPrice)
+
+// Get bid item price
+router.get('/get_bid_price/:id', orderControllers.getBidPrice)
+
+// Update bid or ask order
+router.post('/update_order', orderControllers.updateOrder)
+
+// Delete bid or ask order
+router.post('/delete_order', orderControllers.deleteOrder)
+
+module.exports = router
