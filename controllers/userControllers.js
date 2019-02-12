a43d896a6a4f62273c69a1c58954c8ab496255aa
Panjapol Sanguanpanich <panjaphol@hotmail.com>
2019-02-12 18:44:10 +0700

    Update validate email

--- controllers/userControllers.js
+++ controllers/userControllers.js
@@ -2,6 +2,7 @@ const connection = require('../models/dbConnection')
 const keys = require('../config/keys')
 const bcrypt = require('bcrypt')
 const jwt = require('jsonwebtoken')
+const validator = require('email-validator')
 const saltRounds = 10
 
 const register = (fullName, email, password) => {
@@ -21,44 +22,47 @@ const register = (fullName, email, password) => {
 }
 
 const signUp = (req, res) => {
-    let queryEmail = 'SELECT * FROM User WHERE Email = ?'
-    var checkDup = false
-    connection.query(queryEmail, [req.body.email], (error, results) => {
-        if (error) throw error
-        result = JSON.parse(JSON.stringify(results))
-        if (result.length == 0) {
-            register(req.body.fullName, req.body.email, req.body.password)
-            res.status(200).json({ status: 'success' })
-        } else {
-            res.status(409).json({ status: 'fail' })
-        }
-    })
+    if (validator.validate(req.body.email)) {
+        let queryEmail = 'SELECT * FROM User WHERE Email = ?'
+        connection.query(queryEmail, [req.body.email], (error, results) => {
+            if (error) throw error
+            result = JSON.parse(JSON.stringify(results))
+            if (result.length == 0) {
+                register(req.body.fullName, req.body.email, req.body.password)
+                res.status(200).json({ status: 'success' })
+            } else {
+                res.status(409).json({ status: 'fail' })
+            }
+        })
+    } else res.status(409).json({ status: 'fail' })
 }
 
 const signIn = (req, res) => {
-    let query = 'SELECT * FROM User WHERE Email = ?'
-    let checkPass = false
-    connection.query(query, [req.body.email], (error, results) => {
-        if (error) throw error
-        result = JSON.parse(JSON.stringify(results))
-        if (result.length > 0) {
-            password = result[0].Password
-            checkPass = bcrypt.compareSync(req.body.password, password)
-        }
-        if (checkPass) {
-            let token = jwt.sign({ data: req.body.email }, keys.secretKey, {
-                expiresIn: 60 * 60
-            })
-            res.status(200).json({
-                status: 'success',
-                token: token,
-                userID: result[0].UserID,
-                verified: result[0].Verified
-            })
-        } else {
-            res.status(401).json({ status: 'fail' })
-        }
-    })
+    if (validator.validate(req.body.email)) {
+        let query = 'SELECT * FROM User WHERE Email = ?'
+        let checkPass = false
+        connection.query(query, [req.body.email], (error, results) => {
+            if (error) throw error
+            result = JSON.parse(JSON.stringify(results))
+            if (result.length > 0) {
+                password = result[0].Password
+                checkPass = bcrypt.compareSync(req.body.password, password)
+            }
+            if (checkPass) {
+                let token = jwt.sign({ data: req.body.email }, keys.secretKey, {
+                    expiresIn: 60 * 60
+                })
+                res.status(200).json({
+                    status: 'success',
+                    token: token,
+                    userID: result[0].UserID,
+                    verified: result[0].Verified
+                })
+            } else {
+                res.status(401).json({ status: 'fail' })
+            }
+        })
+    } else res.status(401).json({ status: 'fail' })
 }
 
 const updateProfile = (req, res) => {
