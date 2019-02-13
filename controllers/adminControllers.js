1ecc9749e8463024a2086817d8d98fda821a24ba
Panjapol Sanguanpanich <panjaphol@hotmail.com>
2019-02-13 22:55:21 +0700

    Update verify admin user

--- controllers/adminControllers.js
+++ controllers/adminControllers.js
@@ -1,32 +1,36 @@
 const connection = require('../models/dbConnection')
+const fs = require('fs')
 
 const getAllList = async (req, res) => {
-    if (req.body.userID === 0) {
-        var fs = require('fs')
-        var resultsArray = []
-        var allList = []
-        const dir = fs.readdirSync('./public/cards/')
-        for (const file of dir) {
-            let userID = Number(file.substr(9, 1))
-            let query = 'SELECT * From User WHERE UserID = ?'
-            await new Promise(res => {
-                connection.query(query, [userID], (error, results) => {
-                    let result = JSON.parse(JSON.stringify(results))
-                    const { FullName, Email, Tel, Verified } = result[0]
-                    resultsArray.push({
-                        userID: userID,
-                        fullName: FullName,
-                        email: Email,
-                        tel: Tel,
-                        verified: Verified
-                    })
-                    res()
+    var resultsArray = []
+    var allList = []
+    const dir = fs.readdirSync('./public/cards/')
+    for (const file of dir) {
+        let userID = Number(file.substr(9, 1))
+        let query = 'SELECT * From User WHERE UserID = ?'
+        await new Promise(res => {
+            connection.query(query, [userID], (error, results) => {
+                let result = JSON.parse(JSON.stringify(results))
+                const { FullName, Email, Tel, Verified } = result[0]
+                resultsArray.push({
+                    userID: userID,
+                    fullName: FullName,
+                    email: Email,
+                    tel: Tel,
+                    verified: Verified
                 })
+                res()
             })
-            allList.push(Number(file.substr(9, 1)))
-        }
-        res.status(200).json(resultsArray)
-    } else res.status(401).json({ status: 'fail' })
+        })
+        allList.push(Number(file.substr(9, 1)))
+    }
+    let queryCheck = 'SELECT Verified FROM User WHERE UserID = ?'
+    connection.query(queryCheck, [req.body.userID], (error, results) => {
+        if (error) throw error
+        result = JSON.parse(JSON.stringify(results))
+        if (result[0].Verified == 2) res.status(200).json(resultsArray)
+        else res.status(401).json({ status: 'fail' })
+    })
 }
 
 const verify = (req, res) => {
