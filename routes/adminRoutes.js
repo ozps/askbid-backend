b7bf4a8c4a5b82fc4828d6ac3a0dda580ee50393
Panjapol Sanguanpanich <panjaphol@hotmail.com>
2019-04-06 11:31:42 +0700

    Update Swagger and edit some admin api

--- routes/adminRoutes.js
+++ routes/adminRoutes.js
@@ -43,12 +43,12 @@ router.post('/get_card_image', (req, res) => {
     if (adminControllers.checkAdmin(req.body.level)) {
         let pathImg = __dirname + '/../public/cards/' + req.body.cardImage
         if (fs.existsSync(pathImg)) {
-            res.status(200).json({
+            return res.status(200).json({
                 status: 'success',
                 image64: base64_encode(pathImg)
             })
-        } else res.status(404).json({ status: 'fail' })
-    } else res.status(401).json({ status: 'fail' })
+        } else return res.status(404).json({ status: 'fail' })
+    } else return res.status(401).json({ status: 'fail' })
 })
 
 // Verify
@@ -62,23 +62,25 @@ router.post('/add_item', adminControllers.addItem)
 
 // Upload new item image
 router.post('/upload_item_image', upload, (req, res) => {
-    if (req.file) {
-        let fileName =
-            'sneaker_' +
-            String(req.body.itemId) +
-            path.extname(req.file.originalname)
-        adminControllers.addImage(fileName, req.body.itemId)
-        gm('./public/images/' + fileName)
-            .resize(1100, 740, '!')
-            .write('./public/images/' + fileName, error => {
-                if (!error) {
-                    console.log('Image resized')
-                }
-            })
+    if (adminControllers.checkAdmin(Number(req.body.level))) {
+        if (req.file) {
+            let fileName =
+                'sneaker_' +
+                String(req.body.itemId) +
+                path.extname(req.file.originalname)
+            adminControllers.addImage(fileName, req.body.itemId)
+            gm('./public/images/' + fileName)
+                .resize(1100, 740, '!')
+                .write('./public/images/' + fileName, error => {
+                    if (!error) {
+                        console.log('Image resized')
+                    }
+                })
 
-        return res.status(200).json({ status: 'success' })
-    }
-    return res.status(406).json({ status: 'fail' })
+            return res.status(200).json({ status: 'success' })
+        }
+        return res.status(406).json({ status: 'fail' })
+    } else return res.status(401).json({ status: 'fail' })
 })
 
 module.exports = router
